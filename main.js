45d13d45ae9ecf5cb079483a98e66f3fcf1d07c8
Johnathan31 <ay4718636@gmail.com>
2025-12-04 02:31:41 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+const no = document.getElementById('no');
+const yes = document.getElementById('yes');
+const result = document.getElementById('result');
+
+yes.onclick = () => {
+    result.showModal()
+    setTimeout(() => result.close(), 3000)
+}
+no.onclick = () => {
+  const random = Math.floor(Math.random * 300);
+  
+  no.style.left = `${Math.floor(Math.random() * 300)}px`;
+  no.style.top = `${Math.floor(Math.random() * 300)}px`;
+  no.style.transition = '.5s ease';
+}
